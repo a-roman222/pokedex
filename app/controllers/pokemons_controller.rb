ba7536b6eae94095af525386a2e6c975217b5684
Andres <aroman222ca@gmail.com>
2024-10-20 19:59:21 -0500

    search bar

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,12 +1,19 @@
 class PokemonsController < ApplicationController
   def index
-    @pokemons = Pokemon.all
+    if params[:search].present?
+      @pokemons = Pokemon.where("name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(20)
+
+      if @pokemons.empty?
+        flash.now[:alert] = "No Pokémon found for '#{params[:search]}'."
+      end
+    else
+      @pokemons = Pokemon.page(params[:page]).per(20)
+    end
   end
 
   def show
     @pokemon = Pokemon.find(params[:id])
-
-    rescue ActiveRecord::RecordNotFound
-      redirect_to not_found_path
+  rescue ActiveRecord::RecordNotFound
+    redirect_to not_found_path
   end
 end
