dcc19f366446698d9421af375b3ce27d612cd190
Andres <aroman222ca@gmail.com>
2024-10-20 23:16:49 -0500

    bulma css

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,11 +1,14 @@
 class PokemonsController < ApplicationController
   def index
+    @types = Type.all
+    @generations = Generation.all
+
     if params[:search].present?
       @pokemons = Pokemon.where("name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(20)
-
-      if @pokemons.empty?
-        flash.now[:alert] = "No Pokémon found for '#{params[:search]}'."
-      end
+    elsif params[:type_id].present?
+      @pokemons = Pokemon.joins(:types).where(types: { id: params[:type_id] }).page(params[:page]).per(20)
+    elsif params[:generation_id].present?
+      @pokemons = Pokemon.where(generation_id: params[:generation_id]).page(params[:page]).per(20)
     else
       @pokemons = Pokemon.page(params[:page]).per(20)
     end
